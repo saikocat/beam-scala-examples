d6e529abfe0ed5a6a1fc8aa8ad52d2923f317f86
Duc Hoa, Nguyen <hoameomu@gmail.com>
2019-09-10 17:57:09 +0800

    [wordcount-min] Complete & follow the Java examples faithfully

--- src/main/scala/org/apache/beam/examples/MinimalWordCount.scala
+++ src/main/scala/org/apache/beam/examples/MinimalWordCount.scala
@@ -3,58 +3,37 @@ package org.apache.beam.examples
 import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.io.TextIO
 import org.apache.beam.sdk.options.{PipelineOptions, PipelineOptionsFactory}
-import org.apache.beam.sdk.transforms.{Count, Filter, FlatMapElements, MapElements}
-import org.apache.beam.sdk.transforms.{ParDo, DoFn, SimpleFunction, SerializableFunction, ProcessFunction}
-import org.apache.beam.sdk.transforms.DoFn.{Element, ProcessElement, OutputReceiver}
+import org.apache.beam.sdk.transforms.{Count, Filter, FlatMapElements, MapElements, ProcessFunction}
 import org.apache.beam.sdk.values.{KV, TypeDescriptors}
 
 object MinimalWordCount {
-
-  type JLong = java.lang.Long
-
   def main(args: Array[String]): Unit = {
-
     val options: PipelineOptions = PipelineOptionsFactory.create()
     val pipeline: Pipeline = Pipeline.create(options)
 
+    // Follow the Java guide faithfully, but personally I'd rather make those
+    // anonymous ProcessFunction into individual atomic func for testability
     pipeline
       .apply(TextIO.read().from("/tmp/data-wc.txt"))
-      .apply(ParDo.of(tokenizeWords))
-      .apply(Filter.by(filterNotEmpty)) // NOPE this shit is not working due to closure and override issues
+      .apply(FlatMapElements
+        .into(TypeDescriptors.strings())
+        .via(new ProcessFunction[String, JIterable[String]] {
+          import scala.collection.JavaConverters._
+          override def apply(input: String): JIterable[String] =
+            input.split("[^\\p{L}]+").toIterable.asJava
+        }))
+      .apply(Filter.by(new ProcessFunction[String, JBoolean] {
+        override def apply(input: String): JBoolean = Boolean.box(input.nonEmpty)
+      }))
       .apply(Count.perElement[String]())
-      .apply(MapElements.into(TypeDescriptors.strings()).via(formatResults))
+      .apply(MapElements
+        .into(TypeDescriptors.strings())
+        .via(new ProcessFunction[KV[String, JLong], String] {
+          override def apply(input: KV[String, JLong]): String =
+            s"${input.getKey}: ${input.getValue}"
+        }))
       .apply(TextIO.write().to("/tmp/minimal-wordcounts"))
 
     pipeline.run().waitUntilFinish()
   }
-
-  // Conceptually of DoFn and SimpleFunction
-  // https://stackoverflow.com/a/50631667
-  // Probably can simplify this with ProcessContext: c.element() & c.output()
-  def tokenizeWords = new DoFn[String, List[String]] {
-    @ProcessElement
-    def processElement(
-        @Element element: String,
-        receiver: OutputReceiver[List[String]]): Unit = {
-      receiver.output(element.split("[^\\p{L}]+").toList)
-    }
-  }
-
-  def formatResults = new SimpleFunction[KV[String, JLong], String] {
-    override def apply(input: KV[String, JLong]): String =
-      s"${input.getKey}: ${input.getValue}"
-  }
-
-  // Doesn't work and compile
-  def filterNotEmpty: SerializableFunction[String, Boolean] = new SerializableFunction[String, Boolean] {
-    override def apply(input: String): Boolean = input.nonEmpty
-  }
-
-  def fck: ProcessFunction[String, java.lang.Boolean] = new NamedProcessFn[String, java.lang.Boolean] {
-    override def apply(input: String): java.lang.Boolean = {
-      filterNotEmpty.asInstanceOf[String => java.lang.Boolean](input)
-    }
-  }
 }
-
-trait NamedProcessFn[T, U] extends ProcessFunction[T, U]
