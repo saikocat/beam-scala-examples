cef8aadd9ed55e14cead0a661c8d92af893996aa
Hoa <hoameomu@gmail.com>
2019-09-12 08:50:31 +0800

    [wordcount] add run and parse options

--- src/main/scala/org/apache/beam/examples/scala/WordCount.scala
+++ src/main/scala/org/apache/beam/examples/scala/WordCount.scala
@@ -1,15 +1,54 @@
 package org.apache.beam.examples.scala
 
 import org.apache.beam.examples.common.ExampleUtils
+import org.apache.beam.sdk.Pipeline
+import org.apache.beam.sdk.io.TextIO
 import org.apache.beam.sdk.metrics.{Counter, Distribution, Metrics}
-import org.apache.beam.sdk.transforms.DoFn
+import org.apache.beam.sdk.options.{
+  Default,
+  Description,
+  PipelineOptions,
+  PipelineOptionsFactory,
+  Validation
+}
 import org.apache.beam.sdk.transforms.DoFn.{Element, OutputReceiver, ProcessElement}
-import org.apache.beam.sdk.transforms.SimpleFunction
-import org.apache.beam.sdk.values.KV
+import org.apache.beam.sdk.transforms.{Count, DoFn, MapElements, PTransform, ParDo, SimpleFunction}
+import org.apache.beam.sdk.values.{KV, PCollection}
 
 object WordCount {
-  def main(args: Array[String]): Unit = {}
+  def main(args: Array[String]): Unit = {
+    val options = PipelineOptionsFactory
+      .fromArgs(args: _*)
+      .withValidation()
+      .as(classOf[WordCountOptions])
 
+    runWordCount(options)
+  }
+
+  trait WordCountOptions extends PipelineOptions {
+    @Description("Path of the file to read from")
+    @Default.String("gs://apache-beam-samples/shakespeare/kinglear.txt")
+    def getInputFile: String
+    def setInputFile(path: String)
+
+    @Description("Path of the file to write to")
+    @Validation.Required
+    def getOutput: String
+    def setOutput(path: String)
+  }
+
+  def runWordCount(options: WordCountOptions): Unit = {
+    val p: Pipeline = Pipeline.create(options)
+
+    p.apply("ReadLines", TextIO.read().from(options.getInputFile))
+      .apply(new CountWords())
+      .apply(MapElements.via(new FormatAsTextFn()))
+      .apply("WriteCounts", TextIO.write().to(options.getOutput))
+
+    p.run().waitUntilFinish()
+  }
+
+  /** Tokenizes lines of text into individual words */
   class ExtractWordsFn extends DoFn[String, String] {
     private final val emptyLines: Counter =
       Metrics.counter(classOf[ExtractWordsFn], "emptyLines")
@@ -27,8 +66,25 @@ object WordCount {
     }
   }
 
+  /** A SimpleFunction that converts a Word and Count into a printable string. */
   class FormatAsTextFn extends SimpleFunction[KV[String, JLong], String] {
     override def apply(input: KV[String, JLong]): String =
       s"${input.getKey}: ${input.getValue}"
+  }
+
+  /**
+    * A PTransform that converts a PCollection containing lines of text into a PCollection of
+    * formatted word counts.
+    */
+  class CountWords extends PTransform[PCollection[String], PCollection[KV[String, JLong]]] {
+    override def expand(lines: PCollection[String]): PCollection[KV[String, JLong]] = {
+      // Convert lines of text into individual words.
+      val words: PCollection[String] = lines.apply(ParDo.of(new ExtractWordsFn()))
+
+      // Count the number of times each word occurs.
+      val wordCounts: PCollection[KV[String, JLong]] = words.apply(Count.perElement())
+      // banally follow java guide
+      wordCounts
+    }
   }
 }
