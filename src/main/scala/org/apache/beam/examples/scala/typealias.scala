ad8ed0717302647158e441e92b8e99101024596d
Hoa <hoameomu@gmail.com>
2019-09-15 00:58:51 +0800

    [java-interop] add JDouble alias for java.lang.Double

--- src/main/scala/org/apache/beam/examples/scala/typealias.scala
+++ src/main/scala/org/apache/beam/examples/scala/typealias.scala
@@ -2,6 +2,7 @@ package org.apache.beam.examples.scala
 
 package object typealias {
   type JBoolean = java.lang.Boolean
+  type JDouble = java.lang.Double
   type JInteger = java.lang.Integer
   type JLong = java.lang.Long
   type JIterable[A] = java.lang.Iterable[A]
