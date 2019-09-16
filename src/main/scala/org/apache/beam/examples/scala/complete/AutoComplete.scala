43904b6bdbdc485b979f58637f62ec290e2220c7
Duc Hoa, Nguyen <hoameomu@gmail.com>
2019-09-16 12:24:41 +0800

    [complete-autocomplete] add AllPrefixes DoFn

--- src/main/scala/org/apache/beam/examples/scala/complete/AutoComplete.scala
+++ src/main/scala/org/apache/beam/examples/scala/complete/AutoComplete.scala
@@ -1,14 +1,28 @@
 package org.apache.beam.examples.scala.complete
 
 import scala.math.Ordered
+import scala.math.min
 import scala.util.matching.Regex
 
 import org.apache.beam.examples.scala.typealias._
 import org.apache.beam.sdk.coders.{AvroCoder, DefaultCoder}
 import org.apache.beam.sdk.transforms.DoFn
 import org.apache.beam.sdk.transforms.DoFn.ProcessElement
+import org.apache.beam.sdk.values.KV
 
 object AutoComplete {
+
+  /** A DoFn that keys each candidate by all its prefixes. */
+  class AllPrefixesFn(minPrefix: JInteger, maxPrefix: JInteger = Integer.MAX_VALUE)
+      extends DoFn[CompletionCandidate, KV[String, CompletionCandidate]] {
+    @ProcessElement
+    def processElement(ctx: ProcessContext): Unit = {
+      val word = ctx.element.value
+      for (prefixLen <- minPrefix.toInt to min(word.length, maxPrefix.toInt)) {
+        ctx.output(KV.of(word.substring(0, prefixLen), ctx.element))
+      }
+    }
+  }
 
   /** Class used to store tag-count pairs. */
   @DefaultCoder(classOf[AvroCoder[CompletionCandidate]])
