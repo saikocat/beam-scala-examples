edffa2554fa2ca011fb6e3c5f1854ab1887c09a3
Hoa <hoameomu@gmail.com>
2019-09-18 16:23:01 +0800

    [complete-streamWordExtract] add StringToRowConverterFn

--- src/main/scala/org/apache/beam/examples/scala/complete/StreamingWordExtract.scala
+++ src/main/scala/org/apache/beam/examples/scala/complete/StreamingWordExtract.scala
@@ -1,8 +1,11 @@
 package org.apache.beam.examples.scala.complete
 
+import scala.collection.JavaConverters._
+
 import org.apache.beam.examples.common.ExampleUtils
 import org.apache.beam.sdk.transforms.DoFn
 import org.apache.beam.sdk.transforms.DoFn.ProcessElement
+import com.google.api.services.bigquery.model.{TableFieldSchema, TableRow, TableSchema}
 
 object StreamingWordExtract {
 
@@ -20,9 +23,21 @@ object StreamingWordExtract {
   /** A DoFn that uppercases a word. */
   class UppercaseFn extends DoFn[String, String] {
     @ProcessElement
-    def processElement(ctx: ProcessContext): Unit = {
+    def processElement(ctx: ProcessContext): Unit =
       ctx.output(ctx.element.toUpperCase)
-    }
+  }
+
+  /** Converts strings into BigQuery rows. */
+  class StringToRowConverterFn extends DoFn[String, TableRow] {
+
+    /** In this example, put the whole string into single BigQuery field. */
+    @ProcessElement
+    def processElement(ctx: ProcessContext): Unit =
+      ctx.output(new TableRow().set("string_field", ctx.element))
+
+    def getSchema(): TableSchema =
+      new TableSchema()
+        .setFields(List(new TableFieldSchema().setName("string_field").setType("STRING")).asJava)
   }
 
 }
