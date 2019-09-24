c67e113d92bca404b510d055b5086def44eb8200
Hoa <hoameomu@gmail.com>
2019-09-24 18:00:46 +0800

    [complete-trafficmaxlaneflow] add ExtractFlowInfoFn - using lazy iterator

--- src/main/scala/org/apache/beam/examples/scala/complete/TrafficMaxLaneFlow.scala
+++ src/main/scala/org/apache/beam/examples/scala/complete/TrafficMaxLaneFlow.scala
@@ -6,6 +6,7 @@ import org.apache.beam.examples.scala.typealias._
 import org.apache.beam.sdk.coders.{AvroCoder, DefaultCoder}
 import org.apache.beam.sdk.transforms.DoFn
 import org.apache.beam.sdk.transforms.DoFn.ProcessElement
+import org.apache.beam.sdk.values.KV
 import org.joda.time.Instant
 import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
 
@@ -69,6 +70,69 @@ object TrafficMaxLaneFlow {
         parsedTimestamp = new Instant(timestampMs)
         if items.length > 0
       } ctx.outputWithTimestamp(ctx.element, parsedTimestamp)
+    }
+  }
+
+  /**
+    * Extract flow information for each of the 8 lanes in a reading, and output as separate tuples.
+    * This will let us determine which lane has the max flow for that station over the span of the
+    * window, and output not only the max flow from that calculation, but other associated
+    * information. The number of lanes for which data is present depends upon which freeway the data
+    * point comes from.
+    *
+    * Timestamp,Station ID,Freeway,Direction of Travel,Station Type,Samples,% Observed,Total Flow,Average Occupancy,Average Speed,
+    * Lane 1 Samples,Lane 1 Flow,Lane 1 Average Occupancy,Lane 1 Average Speed,Lane 1 Observed,
+    * Lane 2 Samples,Lane 2 Flow,Lane 2 Average Occupancy,Lane 2 Average Speed,Lane 2 Observed,
+    * Lane 3 Samples,Lane 3 Flow,Lane 3 Average Occupancy,Lane 3 Average Speed,Lane 3 Observed,
+    * Lane 4 Samples,Lane 4 Flow,Lane 4 Average Occupancy,Lane 4 Average Speed,Lane 4 Observed,
+    * Lane 5 Samples,Lane 5 Flow,Lane 5 Average Occupancy,Lane 5 Average Speed,Lane 5 Observed,
+    * Lane 6 Samples,Lane 6 Flow,Lane 6 Average Occupancy,Lane 6 Average Speed,Lane 6 Observed,
+    * Lane 7 Samples,Lane 7 Flow,Lane 7 Average Occupancy,Lane 7 Average Speed,Lane 7 Observed,
+    * Lane 8 Samples,Lane 8 Flow,Lane 8 Average Occupancy,Lane 8 Average Speed,Lane 8 Observed
+    * 01/01/2010 00:00:00,1100310,5,N,FR,18,0,-1,,,9,,,,0,9,,,,0,,,,,0,,,,,0,,,,,0,,,,,0,,,,,0,,,,,0
+    * 01/01/2010 00:00:00,1108148,15,S,ML,54,100,63,.0092,70.9,9,3,.002,76.5,1,9,20,.0167,74,1,9,18,.0163,72,1,9,9,.0077,69.2,1,9,7,.0068,64.5,1,9,6,.0056,64.5,1,,,,,0,,,,,0
+    */
+  class ExtractFlowInfoFn extends DoFn[String, KV[String, LaneInfo]] {
+    private val INVALID_INPUT_LENGTH = 48
+    private val NUM_OF_LANES = 8
+
+    @ProcessElement
+    def processElement(ctx: ProcessContext): Unit = {
+      val items = ctx.element.split(",", -1);
+      items.length match {
+        case length if length < INVALID_INPUT_LENGTH => ()
+        case _ => {
+          // extract the sensor information for the lanes from the input string fields.
+          extractLaneInfo(items).foreach(laneInfo =>
+            ctx.output(KV.of(laneInfo.stationId, laneInfo)))
+        }
+      }
+    }
+
+    def extractLaneInfo(items: Array[String]): Iterator[LaneInfo] = {
+      val timestamp = items(0)
+      val stationId = items(1)
+      val freeway = items(2)
+      val direction = items(3)
+      val totalFlow = Try(items(7).toInt).toOption.getOrElse(-1)
+      for {
+        i <- (1 to NUM_OF_LANES).iterator
+        laneFlow = Try(items(6 + 5 * i).toInt).toOption
+        laneAvgOccupancy = Try(items(7 + 5 * i).toDouble).toOption
+        laneAvgSpeed = Try(items(8 + 5 * i).toDouble).toOption
+        if laneFlow.nonEmpty && laneAvgOccupancy.nonEmpty && laneAvgSpeed.nonEmpty
+      } yield {
+        new LaneInfo(
+          stationId,
+          s"lane $i",
+          direction,
+          freeway,
+          timestamp,
+          laneFlow.get,
+          totalFlow,
+          laneAvgOccupancy.get,
+          laneAvgSpeed.get)
+      }
     }
   }
 }
