a8ded0cf1d66fe743eca6caa586326727b1523a0
Hoa <hoameomu@gmail.com>
2019-09-29 21:26:55 +0800

    [game-utils] fix wrong package name

--- src/main/scala/org/apache/beam/examples/scala/complete/game/utils/WriteToText.scala
+++ src/main/scala/org/apache/beam/examples/scala/complete/game/utils/WriteToText.scala
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.examples.complete.game.utils
+package org.apache.beam.examples.scala.complete.game.utils
 
 import java.io.Serializable
 import java.util.TimeZone
